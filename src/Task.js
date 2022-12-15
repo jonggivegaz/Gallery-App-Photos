069c169ddaa30e534bb6dd2df00820fae522a514
Jonggi Vegas Sitorus <jonggisitorus08@gmail.com>
2022-12-15 20:18:30 +0700

    add redirects

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://my-gallery-app-photos.netlify.app/" // TODO: replace this
 const studentName = "Jonggi Vegas Sitorus" // TODO: replace this
 const studentId = "FE3037724" // TODO: replace this
 
