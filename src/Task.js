895a7f243663c15f8684adad9d2c8ee3e380ac73
Jonggi Vegas Sitorus <jonggisitorus08@gmail.com>
2022-12-15 20:07:13 +0700

    add gallery app photos

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Jonggi Vegas Sitorus" // TODO: replace this
+const studentId = "FE3037724" // TODO: replace this
+
+export { url, studentName, studentId }
